83e893bec87195c5ad685e0d8b4377113e1b2ad7
kim08237 <73979650+kim08237@users.noreply.github.com>
2022-03-07 18:42:34 +0900

    Update FirstClass.java

--- FirstClass.java
+++ FirstClass.java
@@ -4,7 +4,10 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("dear my deer");
-		System.out.print("dear my friend");
+		System.out.println("dear my friend");
+		
+		int i_no = 20;
+		System.out.println("Hello "+ i_no);
 	}
 
 }
