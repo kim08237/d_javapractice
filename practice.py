7cb8a5f29e7fb96ec3443bb9bac9ab9b4953303d
kim08237 <73979650+kim08237@users.noreply.github.com>
2022-03-07 09:15:46 +0900

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,35 @@
+# string= input("입력하세>>>       ")
+# print(string)
+
+# print(type(13.000000))
+
+# string=23
+# inti=float(string)
+# print(inti)
+# print(type(inti))
+
+# string= "{} sdfsdf {}, {}".format(10,20, 30)
+# print(string)
+
+# a='10   20'
+# print(a)
+
+# a= "{}, {}".format(4, type(5))
+# print(a)
+
+# a=input("숫자: ")
+# print(a)
+# print(type(a))
+
+
+# asd=input("n=1: ")
+# ab=int(asd)
+# if ab>0:
+#     raise NotImplementedError
+# else:
+#    pass
+
+list=[[1,2,3],[4,5,6,7],[8,9]]
+for a in list:
+    for b in a:
+        print(b)
